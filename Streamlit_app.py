b51d521adc37948dd995fbee7a244ddbb89ad4b5
Paritosh1947 <132364464+Paritosh1947@users.noreply.github.com>
2023-05-04 06:06:30 -0400

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
 streamlit.title('Welcome to first note')
-streamlite.header('This is header')
+streamlit.header('This is header')
 streamlit.text('This is texr')
 streamlit.text('This is new text')
