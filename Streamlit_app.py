e02f9cfac34c07dc2af6f6721a0f7949ad811f07
Paritosh1947 <132364464+Paritosh1947@users.noreply.github.com>
2023-05-08 11:17:55 -0400

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import snowflake.connector
 import pandas
+import requests
 
 streamlit.title('healthy food')
 streamlit.header('Breakfast Menu')
@@ -47,4 +48,9 @@ streamlit.dataframe(fruityvice_normalized)
 # fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 # streamlit.write('The user entered ', fruit_choice)
 
-
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
